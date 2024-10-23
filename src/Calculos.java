4591f71ea98d54f88b3356165bcb9e36e6f1d2a1
SpitSmoke <jmfelicio.sp@gmail.com>
2024-10-23 15:43:05 -0300

    Finalizando a calculadora de media

---
+++ src/Calculos.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        double nota1, nota2, nota3, nota4;
+
+
+        System.out.println("Digite a primeira nota:");
+        nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota:");
+        nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota:");
+        nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota:");
+        nota4 = scanner.nextDouble();
+
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
